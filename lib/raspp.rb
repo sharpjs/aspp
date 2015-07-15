d0de2e13b232b617e529a6075e291ec2c6dabcd5
Jeff Sharp <jeff.sharp@git>
2015-07-15 15:31:38 -0500

    aspp -> raspp

--- lib/raspp.rb
+++ lib/raspp.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/env ruby
 #
-# aspp.rb - Assembly Preprocessor in Ruby
+# raspp.rb - Assembly Preprocessor in Ruby
 # Copyright (C) 2015 Jeffrey Sharp
 #
-# aspp is free software: you can redistribute it and/or modify it
+# raspp is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License,
 # or (at your option) any later version.
 #
-# aspp is distributed in the hope that it will be useful, but
+# raspp is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
 # the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with aspp.  If not, see <http://www.gnu.org/licenses/>.
+# along with raspp.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Features:
 # - ruby code
@@ -22,7 +22,7 @@
 # - aliases (inline token macros)
 # - comment removal
 
-module ASPP
+module Raspp
   def self.process(input, file = "(stdin)", line = 1)
     Preprocessor.new(file, line).process(input)
   end
