6ddbe513b40fdce4ba3aacbf3146ceb580902d6a
Jeff Sharp <jeff.sharp@git>
2016-02-27 15:24:09 -0600

    Third alternative idea.

---
+++ lib/raspp3.rb
@@ -0,0 +1,230 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+# frozen_string_literal: true
+#
+# raspp - Assembly Preprocessor in Ruby
+# Copyright (C) 2016 Jeffrey Sharp
+#
+# raspp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# raspp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with raspp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Raspp
+  def self.process(input, file = "(stdin)", line = 1)
+    Preprocessor.new(file, line).process(input)
+  end
+
+  private
+
+  WS  = '[ \t]'
+  ID  = '(?> (?!\d) [\w.$]++ )'
+
+  BQ    = %r{ ` (?: [^`]           )*+ `?+ }mx
+  SQ    = %r{ ' (?: [^'\\] | \\.?+ )*+ '?+ }mx
+  DQ    = %r{ " (?: [^"\\] | \\.?+ )*+ "?+ }mx
+  QUOTE = %r{ #{SQ} | #{DQ} | #{BQ} }mx
+
+  EOL    = / \n | \r\n?+ /mx
+  INDENT = / \A #{WS}*+  /mx
+
+  # Code with balanced punctuators
+  CODE = %r{
+    (?<code>
+      (?:
+        [^\[\]\(\)\{\}`'"] |
+        #{QUOTE}       |
+        \[ \g<code> \] |
+        \( \g<code> \) |
+        \{ \g<code> \}
+      )*+
+    )
+  }mx
+
+  class Preprocessor
+    def initialize(file = "(stdin)", line = 1)
+      @file, @line, @scope = file, line, {}
+    end
+
+    def process(input)
+      each_line(input) do |n, line|
+        puts "#{n}: |#{expand!(line)}|"
+      end
+    end
+
+    # Phase 1: Line Processing
+
+    # Logical lines (after contiunation and comment removal)
+    LINES = %r{
+      \G (?!\z)
+      (?<text> (?: [^ \t\r\n`'";] | [ \t]++(?!;) | #{QUOTE} )*+ )
+      (?:      [ \t]*+ ; [^\r\n]*+ )?+
+      (?<eol>  #{EOL} | \z )
+    }mx
+
+    def each_line(input)
+      index  = 1   # line number
+      height = 1
+      prior  = nil # continued line
+
+      input.scan(LINES) do |text, eol|
+        # Count raw lines
+        text.scan(EOL) { height += 1 }
+
+        # Apply prior line continuation
+        if prior
+          text.sub!(INDENT, '')
+          text = prior << text
+        end
+
+        # Check for line continuation
+        if text.chomp!('\\')
+          prior = text
+          next
+        end
+
+        # Pass to next stage
+        yield index, text
+
+        # Advance position
+        index += height
+        height = 1
+        prior  = nil
+      end
+
+      # In case of line continuation at EOF
+      if prior
+        yield index, prior
+      end
+    end
+
+    # Phase 2: Macro Expansion
+
+    # Function-like macro invocation
+    INLINE = %r{
+      (?<name>#{ID}) (?: #{WS}*+ \( (?<args>#{CODE}) \) )?+
+    | 
+      #{QUOTE} (?# exempt from expansion #)
+    }mx
+
+    # Statement-like macro invocation
+    STMT = %r{
+      \A
+      (?<indent> #{INDENT} )
+      (?<labels> (?: #{ID}: #{WS}*+ )*+ )
+      (?<name>   #{ID} )
+      (?:
+        #{WS}++
+        (?<args> #{CODE} )
+      )?
+      \z
+    }mx
+
+    def expand!(text)
+      # Expand function-like macro invocations
+      text.gsub!(INLINE) do |text|
+        "hi!"
+      end
+      text
+    end
+
+    #def process(input)
+    #  prior = nil
+
+    #  input.scan(LINES) do |text, eol|
+    #    # Count real lines in this logical line
+    #    height = 1
+    #    text.scan(EOL) { height += 1 }
+
+    #    # Remove comment
+    #    text.gsub!(COMMENT, '\\k<keep>')
+
+    #    # Apply prior line continuation
+    #    if prior
+    #      text.sub!(INDENT, '')
+    #      text = prior << text
+    #    end
+
+    #    # Expand function-like macro invocations
+    #    text.gsub!(FCALL) { |text|
+    #      puts "#{$~[:name].inspect}: #{$~[:args].inspect} [#{$~[:code].inspect}]"
+
+    #      if $~[:name]
+    #        "fcall"
+    #      else
+    #        text
+    #      end
+    #    }
+
+    #    # Expand directive-like macro invocations
+    #    text.gsub!(DCALL) { |text|
+    #      puts "#{$~[:name].inspect}: #{$~[:args].inspect} at #{$~[:labels].inspect}"
+
+    #      if $~[:name]
+    #        "dcall"
+    #      else
+    #        text
+    #      end
+    #    }
+
+    #    # Check for line continuation
+    #    if text.end_with?('\\')
+    #      prior = text[0..-2]
+    #    else
+    #      prior = nil
+    #      print "|#{text}|", eol
+    #    end
+
+    #    # Advance position
+    #    @line += height
+    #  end
+    #end
+  end
+end
+
+#
+# FEATURES
+#
+# * ; comments
+# * line continuation
+#
+# - Directive-like macros
+# - Function-like macros
+# - Token-like macros
+# - Inline token-like macro def
+# - Inline code
+#
+# - scoped macros
+# - scoped labels
+# -
+# - Replace [ ] with ()
+# - Add # to numbers
+# - Replace ++ and --
+# - Replace 'a' with 'a
+#
+#
+# macros[name] => string
+#              || { |args| expr }
+#
+# .align
+# .scope fpc_send_byte
+#
+
+if __FILE__ == $0
+  # Running as script
+  loop do
+    Raspp::process(ARGF.file.read, ARGF.filename)
+    ARGF.skip
+    break if ARGV.empty?
+  end
+end
+
