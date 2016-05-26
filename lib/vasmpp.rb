2206909ec2551dc21a8b89858ecd59e4380190e5
Jeff Sharp <jeff.sharp@git>
2016-05-26 06:17:49 -0500

    Start yet another Ruby-based preprocessor idea.

---
+++ lib/vasmpp.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+# frozen_string_literal: true
+#
+# vasmpp - A Preprocessor for VASM
+# Copyright (C) 2016 Jeffrey Sharp
+#
+# vasmpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# vasmpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with vasmpp.  If not, see <http://www.gnu.org/licenses/>.
+#
+# IMPLEMENTED FEATURES
+#
+# (none)
+#
+# FUTURE FEATURES:
+#
+# - Inline aliases
+# ?
+#
+
+module Vasmpp
+  class Processor
+    def process(input, output, name = "(stdin)", line = 1)
+      @in   = input
+      @out  = output
+      @name = name
+      @line = line
+
+      @out.write(@in)
+    end
+  end
+end # Vasmpp
+
+if __FILE__ == $0
+  # Running as script
+  trap "PIPE", "SYSTEM_DEFAULT"
+  processor = Vasmpp::Processor.new
+  loop do
+    processor.process(ARGF.file.read, $stdout, ARGF.filename)
+    ARGF.skip
+    break if ARGV.empty?
+  end
+end
+
