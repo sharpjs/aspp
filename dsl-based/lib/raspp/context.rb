49d092e93a8728b2bcf2a02af67a16d105077d90
Jeff Sharp <jeff.sharp@git>
2016-05-31 09:21:37 -0500

    Comment out in prep for some rework.

--- dsl-based/lib/raspp/context.rb
+++ dsl-based/lib/raspp/context.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 #
 # This file is part of Raspp.
 # Copyright (C) 2016 Jeffrey Sharp
@@ -16,116 +17,116 @@
 # along with Raspp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-require_relative 'block'
-require_relative 'refinements'
+#require_relative 'block'
+#require_relative 'refinements'
 
 module Raspp
-  using self
+  #using self
 
   class Context
-    attr_reader :_parent, :_local_prefix, :_local_index, :_out_stream
+    #attr_reader :_parent, :_local_prefix, :_local_index, :_out_stream
 
-    def initialize(parent = nil, name = nil, out = nil)
-      @_parent       = parent
-      @_local_prefix = parent ? "#{parent.local(name)}." : ".L."
-      @_local_index  = 0
-      @_out_stream   = out || parent && parent._out_stream || $stdout
-    end
+    #def initialize(parent = nil, name = nil, out = nil)
+    #  @_parent       = parent
+    #  @_local_prefix = parent ? "#{parent.local(name)}." : ".L."
+    #  @_local_index  = 0
+    #  @_out_stream   = out || parent && parent._out_stream || $stdout
+    #end
 
-    def method_missing(sym, *args, &block)
-      @_parent ? @_parent.send(sym, *args, &block) : super
-    end
+    #def method_missing(sym, *args, &block)
+    #  @_parent ? @_parent.send(sym, *args, &block) : super
+    #end
 
-    def respond_to_missing?(sym, all)
-      @_parent && @_parent.respond_to?(sym, all)
-    end
+    #def respond_to_missing?(sym, all)
+    #  @_parent && @_parent.respond_to?(sym, all)
+    #end
 
-    def printf (*args) @_out_stream.printf *args end
-    def print  (*args) @_out_stream.print  *args end
-    def puts   (*args) @_out_stream.puts   *args end
-    def putc   (*args) @_out_stream.putc   *args end
+    #def printf (*args) @_out_stream.printf *args end
+    #def print  (*args) @_out_stream.print  *args end
+    #def puts   (*args) @_out_stream.puts   *args end
+    #def putc   (*args) @_out_stream.putc   *args end
 
-    def equ (sym, val) dir :".equ", sym, val; sym end
-    def eqv (sym, val) dir :".eqv", sym, val; sym end
-    def set (sym, val) dir :".set", sym, val; sym end
+    #def equ (sym, val) dir :".equ", sym, val; sym end
+    #def eqv (sym, val) dir :".eqv", sym, val; sym end
+    #def set (sym, val) dir :".set", sym, val; sym end
 
+    ###
+    ## Defines a label.
     ##
-    # Defines a label.
-    #
-    # If +sym+ is given, it is used as the label name.
-    # Otherwise, a unique local label is generated.
-    #
-    # If a block is given, this method defines a code block
-    # with both start and end labels.
-    #
-    def at(sym = nil)
-      sym = sym ? sym.to_symbol(self) : local
-      puts "#{sym}:"
-      if block_given?
-        yield block = Block.new(sym)
-        puts "#{block.end}:" if block.end_used?
-      end
-      sym
-    end
+    ## If +sym+ is given, it is used as the label name.
+    ## Otherwise, a unique local label is generated.
+    ##
+    ## If a block is given, this method defines a code block
+    ## with both start and end labels.
+    ##
+    #def at(sym = nil)
+    #  sym = sym ? sym.to_symbol(self) : local
+    #  puts "#{sym}:"
+    #  if block_given?
+    #    yield block = Block.new(sym)
+    #    puts "#{block.end}:" if block.end_used?
+    #  end
+    #  sym
+    #end
 
-    def global(sym)
-      sym = sym.to_symbol(self)
-      puts ".global #{sym}"
-      sym
-    end
+    #def global(sym)
+    #  sym = sym.to_symbol(self)
+    #  puts ".global #{sym}"
+    #  sym
+    #end
 
-    def local(sym = nil)
-      if sym.nil?
-        sym = @_local_index += 1
-      else
-        sym = sym.to_s
-        sym.slice! /^[$@]/
-      end
-      :"#{@_local_prefix}#{sym}"
-    end
+    #def local(sym = nil)
+    #  if sym.nil?
+    #    sym = @_local_index += 1
+    #  else
+    #    sym = sym.to_s
+    #    sym.slice! /^[$@]/
+    #  end
+    #  :"#{@_local_prefix}#{sym}"
+    #end
 
-    def skip(count, fill = nil)
-      dir :".skip", count, fill
-    end
+    #def skip(count, fill = nil)
+    #  dir :".skip", count, fill
+    #end
 
-    def skip_to(offset, fill = nil)
-      dir :".org", offset, fill
-    end
+    #def skip_to(offset, fill = nil)
+    #  dir :".org", offset, fill
+    #end
 
-    def incbin(path, start, len)
-      dir :".incbin", path, start, len
-    end
+    #def incbin(path, start, len)
+    #  dir :".incbin", path, start, len
+    #end
 
-    [:byte, :word, :long].each do |type|
-      define_method type do |*values|
-        values = [0] if values.empty?
-        values.map! { |v| v.to_asm }
-        puts "\t.#{type}\t#{values.join(', ')}"
-      end
-    end
+    #[:byte, :word, :long].each do |type|
+    #  define_method type do |*values|
+    #    values = [0] if values.empty?
+    #    values.map! { |v| v.to_asm }
+    #    puts "\t.#{type}\t#{values.join(', ')}"
+    #  end
+    #end
 
-    [:ascii, :string].each do |type|
-      define_method type do |*values|
-        unless values.empty?
-          values.map! { |v| v.to_s.to_asm }
-          puts "\t.#{type}\t#{values.join(', ')}"
-        end
-      end
-    end
+    #[:ascii, :string].each do |type|
+    #  define_method type do |*values|
+    #    unless values.empty?
+    #      values.map! { |v| v.to_s.to_asm }
+    #      puts "\t.#{type}\t#{values.join(', ')}"
+    #    end
+    #  end
+    #end
 
-    def struct(sym = nil)
-      addr 0
-      yield if block_given?
-    end
+    #def struct(sym = nil)
+    #  addr 0
+    #  yield if block_given?
+    #end
 
-    # Writes a directive
-    def dir(op, *args)
-      args.compact!
-      args.map! { |a| a.to_asm }
-      puts args.empty? \
-        ? "\t#{op}"
-        : "\t#{op}\t#{args.join(', ')}"
-    end
+    ## Writes a directive
+    #def dir(op, *args)
+    #  args.compact!
+    #  args.map! { |a| a.to_asm }
+    #  puts args.empty? \
+    #    ? "\t#{op}"
+    #    : "\t#{op}\t#{args.join(', ')}"
+    #end
   end
 end
 
--- dsl-based/lib/raspp/top_level.rb
+++ dsl-based/lib/raspp/top_level.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 #
 # This file is part of Raspp.
 # Copyright (C) 2016 Jeffrey Sharp
@@ -16,63 +17,63 @@
 # along with Raspp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-require_relative 'refinements'
-require_relative 'operand'
+#require_relative 'refinements'
+#require_relative 'operand'
 require_relative 'context'
-require_relative 'section'
+#require_relative 'section'
 
 module Raspp
-  using self
+  #using self
 
   class TopLevel < Context
-    def initialize(out = nil)
-      super(nil, nil, out)
-    end
+    #def initialize(out = nil)
+    #  super(nil, nil, out)
+    #end
 
-    def text; puts ".text"; end
-    def data; puts ".data"; end
-    def bss;  puts ".bss";  end
+    #def text; puts ".text"; end
+    #def data; puts ".data"; end
+    #def bss;  puts ".bss";  end
 
-    def section(sym, &body)
-      sec = Section.new(sym)
-      sec.instance_exec(&body) if body
-      sec.use
-      sec
-    end
+    #def section(sym, &body)
+    #  sec = Section.new(sym)
+    #  sec.instance_exec(&body) if body
+    #  sec.use
+    #  sec
+    #end
 
-    def addr(addr)
-      dir :".offset", addr
-    end
+    #def addr(addr)
+    #  dir :".offset", addr
+    #end
 
-    # TODO: This is target-specific
-    def func(sym, &body)
-      klass = $frame_type || AppFunction
-      klass.new(self, sym).define(&body)
-      puts
-    end
+    ## TODO: This is target-specific
+    #def func(sym, &body)
+    #  klass = $frame_type || AppFunction
+    #  klass.new(self, sym).define(&body)
+    #  puts
+    #end
 
-    def import(*files)
-      # Get path of the importing file
-      base = caller_locations(1, 1)[0].path
+    #def import(*files)
+    #  # Get path of the importing file
+    #  base = caller_locations(1, 1)[0].path
 
-      # If it's a real file, import relative to it
-      if File.file?(base)
-        base = File.dirname(base)
-        files.map! { |f| File.join(base, f) }
-      end
+    #  # If it's a real file, import relative to it
+    #  if File.file?(base)
+    #    base = File.dirname(base)
+    #    files.map! { |f| File.join(base, f) }
+    #  end
 
-      # Keep track of imported files
-      @_imported ||= {}
+    #  # Keep track of imported files
+    #  @_imported ||= {}
 
-      # Import files
-      files.each do |rel|
-        abs = File.absolute_path(rel)
-        unless @_imported[abs]
-          instance_eval IO.read(rel), rel
-          @_imported[abs] = rel
-        end
-      end
-    end
+    #  # Import files
+    #  files.each do |rel|
+    #    abs = File.absolute_path(rel)
+    #    unless @_imported[abs]
+    #      instance_eval IO.read(rel), rel
+    #      @_imported[abs] = rel
+    #    end
+    #  end
+    #end
   end
 end
 
